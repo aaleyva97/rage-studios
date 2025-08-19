26827b056bbb86df1480f551c9a607a155f3c747
Alvaro <aaleyva97@gmail.com>
2025-08-19 08:18:24 -0700

    change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,22 +7,19 @@ export const routes: Routes = [
     loadComponent: () => import('./features/landing/pages/landing/landing').then(m => m.Landing)
   },
   { 
-    path: 'checkout',
-    children: [
-      {
-        path: 'success',
-        loadComponent: () => import('./features/checkout/pages/success/success').then(m => m.Success)
-      },
-      {
-        path: 'cancel',
-        loadComponent: () => import('./features/checkout/pages/cancel/cancel').then(m => m.Cancel)
-      },
-      {
-        path: ':packageId',
-        loadComponent: () => import('./features/checkout/pages/checkout/checkout').then(m => m.Checkout),
-        canActivate: [authGuard]
-      }
-    ]
+    path: 'checkout/:packageId', 
+    loadComponent: () => import('./features/checkout/pages/checkout/checkout').then(m => m.Checkout),
+    canActivate: [authGuard]
+  },
+  { 
+    path: 'checkout/success', 
+    loadComponent: () => import('./features/checkout/pages/success/success').then(m => m.Success),
+    canActivate: [authGuard]
+  },
+  { 
+    path: 'checkout/cancel', 
+    loadComponent: () => import('./features/checkout/pages/cancel/cancel').then(m => m.Cancel),
+    canActivate: [authGuard]
   },
   {
     path: '**',
