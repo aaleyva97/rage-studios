625262e4f94e9a1db82a718d5d64db3a33d7be57
Alvaro <aaleyva97@gmail.com>
2025-08-22 22:53:52 -0700

    save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   baseUrl: 'https://ragestudios.mx',
   SUPABASE_URL: 'https://qixgxmlpmploaataidnv.supabase.co',
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpeGd4bWxwbXBsb2FhdGFpZG52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyOTExODMsImV4cCI6MjA3MDg2NzE4M30.ItBAlRYQuXTIkihyXejTwSfUTephNOwspseuoMcWpgU',
-  STRIPE_PUBLISHABLE_KEY: process.env['NG_APP_STRIPE_PUBLISHABLE_KEY'] || ''
+  STRIPE_PUBLISHABLE_KEY: 'pk_live_51RsEZC2ET2zubaMpA3aMEBGwKZ7r5Z2exp4cqDtdy9tnNaNKiWs5OQXBw2SWGcCiOYtQxPL3LZgUIq6i5tpWf97800p0Sx6rhI'
 };
