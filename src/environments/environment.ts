b8f419b358bfa1fa8de7ae25f73ab526f7c3c4ae
Alvaro <aaleyva97@gmail.com>
2025-09-08 14:36:15 -0700

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,13 @@ export const environment = {
   baseUrl: 'https://ragestudios.mx',
   SUPABASE_URL: 'https://qixgxmlpmploaataidnv.supabase.co',
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpeGd4bWxwbXBsb2FhdGFpZG52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyOTExODMsImV4cCI6MjA3MDg2NzE4M30.ItBAlRYQuXTIkihyXejTwSfUTephNOwspseuoMcWpgU',
-  STRIPE_PUBLISHABLE_KEY: 'pk_live_51RsEZC2ET2zubaMpA3aMEBGwKZ7r5Z2exp4cqDtdy9tnNaNKiWs5OQXBw2SWGcCiOYtQxPL3LZgUIq6i5tpWf97800p0Sx6rhI'
+  STRIPE_PUBLISHABLE_KEY: 'pk_live_51RsEZC2ET2zubaMpA3aMEBGwKZ7r5Z2exp4cqDtdy9tnNaNKiWs5OQXBw2SWGcCiOYtQxPL3LZgUIq6i5tpWf97800p0Sx6rhI',
+
+    serviceWorker: {
+    enabled: true,
+    script: 'firebase-messaging-sw.js',
+    registrationStrategy: 'registerWhenStable:30000',
+    scope: '/',
+    updateViaCache: 'imports' as 'imports'
+  }
 };
